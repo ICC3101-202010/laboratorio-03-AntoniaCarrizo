107ebec8bcf1bd4d42018ff624855b103839363b
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-04-04 23:38:37 -0300

    poner git ignore

---
+++ Laboratorio3-AntoniaCarrizo/Laboratorio3-AntoniaCarrizo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio3AntoniaCarrizo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
