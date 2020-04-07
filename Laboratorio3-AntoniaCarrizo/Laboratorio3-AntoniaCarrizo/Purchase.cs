41f27270d68739c1e02593bedb97d36897a84a6e
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-04-07 04:39:28 -0400

    falta

--- Laboratorio3-AntoniaCarrizo/Laboratorio3-AntoniaCarrizo/Purchase.cs
+++ Laboratorio3-AntoniaCarrizo/Laboratorio3-AntoniaCarrizo/Purchase.cs
@@ -8,19 +8,44 @@ namespace Laboratorio3AntoniaCarrizo
         private DateTime fechahora;
 
         private List<Purchase> todasCompras = new List<Purchase>();
-        Product pp = new Product();
+        private Product pp = new Product();
+        private Client client = new Client();
+        private Employee employee = new Employee();
+        private List<Product> productoscarro = new List<Product>();
 
-       
-        public Purchase(Client client, Employee employee, List<Product> productoscomprados, DateTime fechahora)
+
+        public Purchase(Client client, Employee employee, List<Product> productoscarro, DateTime fechahora)
         {
-
+            this.fechahora = fechahora;
         }
         
         public Purchase()
         {
 
         }
-        
+        public List<Purchase> TodasCompras()
+        {
+            return todasCompras;
+        }
+        public Client Client()
+        {
+            return client;
+        }
+        public Employee Employee()
+        {
+            return employee;
+        }
+        public DateTime Fechahora()
+        {
+            return fechahora;
+        }
+        public List<Product> Productoscarro()
+        {
+            return productoscarro;
+        }
+
+
+
 
         public void RealizarCompra(string sino,List<Product>carro,Client client,Employee employee)
         {
@@ -46,14 +71,35 @@ namespace Laboratorio3AntoniaCarrizo
 
         public void VerTodaslascompras()
         {
-
-            int n = 1;
-            for (int i = 0; i < todasCompras.Count; i++)
+            if (todasCompras.Count > 0)
             {
-                Console.WriteLine(n + ") " );
-                n++;
+                int n = 1;
+                for (int i = 0; i < todasCompras.Count; i++)
+                {
+                    Console.WriteLine(n + ") \n Cliente: Nombre: " + todasCompras[i].Client().Name() + " ; Apellido: " + todasCompras[i].Client().Lastname() + " ; Rut: " + todasCompras[i].Client().Rut());
+                    Console.WriteLine("Empleado: Nombre: " + todasCompras[i].Employee().Name() + " ; Apellido: " + todasCompras[i].Employee().Lastname() + " ; Rut: " + todasCompras[i].Employee().Rut());
+                    Console.WriteLine("Fecha: " + todasCompras[i].Fechahora().Date + "- Hora: " + todasCompras[i].Fechahora().Hour);
+                    Console.WriteLine("Productos Comprados: \n");
+
+
+                    for (int p = 0; p < productoscarro.Count; p++)
+                    {
+                        int l = 1;
+                        for (int a = 0; a < productoscarro.Count; a++)
+                        {
+                            Console.WriteLine(l + ") " + todasCompras[p].Productoscarro()[a].NameProduct() + "-" + todasCompras[p].Productoscarro()[a].Brand() + "- Cantidad: " + todasCompras[p].Productoscarro()[a].Stock());
+                            l++;
+                        }
+
+                    }
+
+                }
+
             }
-            
+            else
+            {
+                Console.WriteLine("No hay ninguna compra");
+            }
         }
 
 
